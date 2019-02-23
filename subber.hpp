Kuxe/subber

commit 7cdeed6f2256c672bf309e0ee7dd02f8b2cb3bcb

Code cleanups. Renamed 'notify' to 'notified'. Added compile-time checker 'is_subscribed_to'.

subber.hpp
@@ -3,36 +3,60 @@
 
 #include <unordered_set>
 
-/** @brief Subber is a class which subscribes to events
-	Subber (abv. Subscriber) is an abstract class which requires anyconcrete subclass to implement a notify-method.
-	The notify-method is called whenever someone makes a call to the global publish-method
-	if the type of the parametrization and type of argument matches.
-	@author Joakim "Kuxe" Thorén
+/** @brief Subber is a class which subscribes to events.
+	Subber (abv. Subscriber) is an abstract class that requires any concrete
+	subclass to implement a notified-method. The notified-method is called
+	when the global publish function is called with a matching type T.
+	@author Joakim Thorén
 	@date June 2016
  **/
 
-template<class T>
-class SubberHelper {
-	static std::unordered_set<SubberHelper<T>*> subbers;
-protected:
-	constexpr SubberHelper() { subbers.insert(this); }
-	virtual ~SubberHelper() { subbers.erase(this); }
-public:
-	virtual void notify(const T& e) = 0;
-	template<typename U, typename... Us>
-	friend void publish(const U& u, const Us&... us);
-};
-template<typename T> std::unordered_set<SubberHelper<T>*> SubberHelper<T>::subbers;
-template<class... Ts> struct Subber : SubberHelper<Ts>... { };
+namespace subber {
 
-static void publish() { }
-template<typename T, typename... Ts>
-static void publish(const T& t, const Ts&... ts) {
-	for(const auto& subber : SubberHelper<typename std::remove_const<typename std::remove_reference<T>::type>::type>::subbers) {
-		subber->notify(t);
+template<typename event_type>
+class SubberHelper {
+	static std::unordered_set<SubberHelper<event_type>*> subbers;
+protected:
+	constexpr SubberHelper() {
+		static_assert(std::is_object<event_type>::value,
+		"event_type is not an object. Do strip away qualifiers, \
+		 references or pointers when inheriting from Subber!");
+		subbers.emplace(this);
 	}
-	publish(ts...);
+	virtual ~SubberHelper() {
+		subbers.erase(this);
+		}
+public:
+	virtual void notified(const event_type& e) = 0;
+	template<typename T, typename... Ts>
+	friend void publish(const T&, const Ts&...);
+};
+
+template<typename event_type>
+std::unordered_set<SubberHelper<event_type>*> SubberHelper<event_type>::subbers;
+template<typename... event_types>
+struct Subber : private SubberHelper<event_types>... { };
+
+template<typename subber_type, typename event_type>
+struct is_subbed_to : public std::is_base_of<SubberHelper<event_type>, subber_type> { };
+#if __cplusplus >= 201703L
+	template<typename subber_type, typename event_type>
+	inline constexpr bool is_subbed_to_v = is_subbed_to<subber_type, event_type>::value;
+#endif
+
+void publish() { }
+template<typename event_type, typename... event_types>
+void publish(const event_type& event, const event_types&... events) {
+	static_assert(std::is_object<event_type>::value,
+		"event_type is not an object. Do strip away qualifiers, \
+		 references or pointers when calling publish!");
+	using base_type = typename std::remove_cv<typename std::remove_reference<event_type>::type>::type;
+	for(const auto& subber : SubberHelper<base_type>::subbers) {
+		subber->notified(event);
+	}
+	publish(events...);
 }
 
+} //End of namespace subber
 
 #endif //SUBSCRIBER_HPP

.gitignore
@@ -27,3 +27,6 @@
 *.exe
 *.out
 *.app
+
+# Project files
+.vscode

example.cpp
@@ -1,12 +1,13 @@
-#include "subber.hpp"
-
 /** @brief example.cpp illustrates how to use subber.hpp
-	@author Joakim "Kuxe" Thorén
+	@author Joakim Thorén
 	@date December 2016
  **/
-	
+
+#include "subber.hpp"
+using namespace subber;
+
 /** QuitEvent, CircleEvent and CollatzEvent are example events.
-	SimpleEvent is a simple message without data or functions.
+	SimpleEvent is a simple event without data or functions.
 	CircleEvent is an event containing a representation of a circle.
 	CollatzEvent is an event with a member function collatz.
 **/
@@ -14,44 +15,52 @@
 struct SimpleEvent { };
 struct CircleEvent { const float x, y, r; };
 struct CollatzEvent {
-	constexpr int collatz(int num) const {
-		return 	num == 1 ? 0 : num % 2 ? collatz(3*num+1) + 1 : collatz(num/2) + 1;
+	constexpr int collatz(int n) const {
+		return n == 1 ? 0 : n % 2 ? collatz(3*n+1) + 1 : collatz(n/2) + 1;
 	}
 };
 
 /** Foo will be notified by any publication of a SimpleEvent or a CircleEvent **/
 struct Foo : public Subber<SimpleEvent, CircleEvent> {
-	void notify(const SimpleEvent& event) override {
+	void notified(const SimpleEvent& event) override {
 		printf("Foo received SimpleEvent\n");
 	}
 
-	void notify(const CircleEvent& event) override {
-		printf("Foo received CircleEvent, whose area is: %fmm²\n", 3.14f*event.r*event.r);
+	void notified(const CircleEvent& event) override {
+		printf("Foo received CircleEvent, whose radius is: %fmm\n", event.r);
 	}
 };
 
 /** Bar will be notified by any publication of a SimpleEvent or a CollatzEvent **/
 struct Bar : public Subber<SimpleEvent, CollatzEvent> {
-	void notify(const SimpleEvent& event) override {
+	void notified(const SimpleEvent& event) override {
 		printf("Bar received SimpleEvent\n");
 	}
 
-	void notify(const CollatzEvent& event) override {
+	void notified(const CollatzEvent& event) override {
 		printf("Bar received CollatzEvent, collatz(6) = %i\n", event.collatz(6));
 	}
 };
 
+//It is possible to check compile-time if a class is subscribed to some event
+static_assert(is_subbed_to<Foo, SimpleEvent>::value, "Passes since Foo is subbed to SimpleEvent");
+static_assert(is_subbed_to<Foo, CircleEvent>::value, "Passes since Foo is subbed to CircleEvent");
+static_assert(!is_subbed_to<Foo, CollatzEvent>::value, "Passes since Foo is not subbed to CollatzEvent");
+static_assert(is_subbed_to<Bar, SimpleEvent>::value, "Passes since Bar is subbed to SimpleEvent");
+static_assert(!is_subbed_to<Bar, CircleEvent>::value, "Passes since Bar is not subbed to CircleEvent");
+static_assert(is_subbed_to<Bar, CollatzEvent>::value, "Passes since Bar is subbed to CollatzEvent");
+
 int main(int argc, char** argv) {
 
 	SimpleEvent simpleEvent;
-	CircleEvent circleEvent = {1.2f, 3.7f, 1.3f};
+	CircleEvent circleEvent = {1.3f, 3.7f, 1.0f};
 	CollatzEvent collatzEvent;
 
 	//Neither Foo or Bar is instantiated so nothing is printed
 	publish(simpleEvent, circleEvent); 
 	{
 		Foo foo;
-		//"Foo received simpleEvent" is printed and
+		//"Foo received simpleEvent" is printed
 		//but no collatz printout since Foo doesn't inherit from Subber<CollatzEvent>
 		publish(simpleEvent, collatzEvent);
 
@@ -67,7 +76,12 @@ int main(int argc, char** argv) {
 	//No subber is instantiated so expect nothing
 	publish(simpleEvent, circleEvent, collatzEvent);
 
-	//In total Foo should print SimpleEvent twice and circleEvent once
-	//and Bar should print SimpleEvent once and collatz once.
+	/* Expected output:
+		Foo received SimpleEvent
+		Bar received SimpleEvent
+		Foo received SimpleEvent
+		Foo received Circle event, whose radius is 1.0
+		Bar received CollatzEvent, collatz(6) = 8
+	*/
 	return 0;
 }

commit aab67976f76420f586d657cdd14900278e47f4ea

inlined base-case publish to ensure internal linkage, so that multiple definitions linking errors are avoided
This issue was introduced with 7cdeed6f2256c672bf309e0ee7dd02f8b2cb3bcb

subber.hpp
@@ -44,7 +44,7 @@ struct is_subbed_to : public std::is_base_of<SubberHelper<event_type>, subber_ty
 	inline constexpr bool is_subbed_to_v = is_subbed_to<subber_type, event_type>::value;
 #endif
 
-void publish() { }
+inline void publish() { }
 template<typename event_type, typename... event_types>
 void publish(const event_type& event, const event_types&... events) {
 	static_assert(std::is_object<event_type>::value,